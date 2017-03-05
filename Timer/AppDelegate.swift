ca2b77bd1c6f02260eb3d2adb836b7700605dca5
Simon Whitaker <sw@netcetera.org>
2017-03-05 08:30:03 +0000

    Initial Commit

---
+++ Timer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Timer
+//
+//  Created by Simon on 05/03/2017.
+//  Copyright © 2017 Simon Whitaker. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Timer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Simon on 05/03/2017.
+//  Copyright © 2017 Simon Whitaker. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
