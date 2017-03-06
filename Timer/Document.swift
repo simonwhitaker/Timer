987cb872ca710b7fd6716c6f72a7ae8672f4d189
Simon Whitaker <sw@netcetera.org>
2017-03-06 20:08:03 +0000

    Make app document-based

---
+++ Timer/Document.swift
@@ -0,0 +1,38 @@
+//
+//  Document.swift
+//  Timer
+//
+//  Created by Simon on 06/03/2017.
+//  Copyright © 2017 Simon Whitaker. All rights reserved.
+//
+
+import Cocoa
+
+class Document: NSDocument {
+
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+    
+    override func makeWindowControllers() {
+        // Returns the Storyboard that contains your Document window.
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        let windowController = storyboard.instantiateController(withIdentifier: "Document Window Controller") as! NSWindowController
+        self.addWindowController(windowController)
+    }
+
+    override func data(ofType typeName: String) throws -> Data {
+        // Insert code here to write your document to data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning nil.
+        // You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
+        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+    }
+    
+    override func read(from data: Data, ofType typeName: String) throws {
+        // Insert code here to read your document from the given data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning false.
+        // You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead.
+        // If you override either of these, you should also override -isEntireFileLoaded to return false if the contents are lazily loaded.
+        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+    }
+
+}
